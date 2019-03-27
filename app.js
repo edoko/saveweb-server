d0fdc683e3f01089cc8e379f0d88cb9508f696a7
Seongmin Park <wluns32@gmail.com>
2019-03-27 22:22:26 +0900

    로직 수정

--- app.js
+++ app.js
@@ -12,19 +12,21 @@ const session = require('express-session')
 const auth = require('./routes/auth')
 const user = require('./models/user')
 
-const app = express();
+const port = process.env.PORT || 3000
+
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // default settings
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: 'false' }))
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // enable cors
 app.use(cors())
@@ -40,8 +42,8 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 // mongodb connect
-mongoose.connect('mongodb://localhost:27017/saveweb', { useNewUrlParser: true })
-mongoose.Promise = global.Promise
+// mongoose.connect('mongodb://localhost:27017/saveweb', { useNewUrlParser: true })
+// mongoose.Promise = global.Promise
 
 app.use('/api/v1/auth', auth)
 
@@ -49,20 +51,30 @@ passport.use(new LocalStrategy(user.authenticate()))
 passport.serializeUser(user.serializeUser())
 passport.deserializeUser(user.deserializeUser())
 
+// main
+app.get('/', (req, res) => {
+  res.send( "Hello world!" )
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500)
+  res.render('error')
 });
 
-module.exports = app;
+// start server
+app.listen(port, () => {
+  console.log( `server started at http://localhost:${ port }` )
+})
+
+module.exports = app
